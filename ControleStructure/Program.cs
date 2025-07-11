3a2563b8ebbfd165d2ba53f0cccdef9e6baa5d14
Arif Mahmud <k.i.d.s69@outlook.com>
2025-07-11 23:58:59 +0600

    If , else if , if else if , switch example added

--- ControleStructure/Program.cs
+++ ControleStructure/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string Name = "Arif Mahmu";
+            string Name = "Arif Mahmud";
 
             if (Name == "Arif Mahmud")
             {
@@ -13,6 +13,62 @@
             else
             {
                 Console.WriteLine("Hello Unknown User");
+            }
+
+
+            Console.Write("Enter a number: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            // Using if-else-if ladder
+            if (i == 10)
+                Console.WriteLine("i is 10");
+            else if (i == 15)
+                Console.WriteLine("i is 15");
+            else if (i == 20)
+                Console.WriteLine("i is 20");
+            else
+                Console.WriteLine("i is not present");
+
+            int j = 10;
+
+            if (j == 10)
+            {
+
+                // nested - if statement
+                // will only be executed if statement
+                // above it is true
+                if (j < 12)
+                    Console.WriteLine("j is smaller than 12 too");
+                else
+                    Console.WriteLine("j is greater than 12");
+            }
+
+            // Ask for user's name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Ask for a number
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Print welcome message
+            Console.WriteLine($"\nHello {name}, you entered {number}.");
+
+            // Using Switch case 
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("This is case 10");
+                    break;
+                case 20:
+                    Console.WriteLine("This is case 20");
+                    break;
+                case 30:
+                    Console.WriteLine("This is case 30");
+                    break;
+                default:
+                    Console.WriteLine("None of the cases match your number.");
+                    break;
             }
         }
     }
