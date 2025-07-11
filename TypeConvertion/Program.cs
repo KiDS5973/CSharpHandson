9f93fe0f9a49ceec6a70a7cc88998ff1ffc02eb0
Arif Mahmud <k.i.d.s69@outlook.com>
2025-07-11 15:05:04 +0600

    Type convertion example added

--- TypeConvertion/Program.cs
+++ TypeConvertion/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int myInt = 105;
+            double myDouble = myInt; // Implicit conversion from int to double
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble); // Output: 105.0
+
+            double myDouble2 = 105.5;
+            int i =(int)myDouble2; // Explicit conversion from double to int
+            Console.WriteLine(i);
+
+            string str = "123";
+            int num = Convert.ToInt32(str); // Convert string to int
+            Console.WriteLine(num); // Output: 123
+
+            string str2 = "567";
+            int num2 = int.Parse(str2); // Parse string to int
+            Console.WriteLine(num2); // Output: 567
         }
     }
 }
